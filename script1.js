d68144bfad46cd35acdf679eff0d0a4300acb4fb
ledemetrio <160793915+ledemetrio@users.noreply.github.com>
2025-09-11 22:27:27 -0300

    ok

--- script1.js
+++ script1.js
@@ -75,3 +75,18 @@ const perguntas=[
 ];
 
 
+let atual = 0;
+let perguntaAtual;
+let historiaFinal="";
+
+function mostraPergunta(){
+    if(atual >=perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual=perguntas[atual];
+    caixasPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent="";
+    mostraAlternativas();
+
+}
